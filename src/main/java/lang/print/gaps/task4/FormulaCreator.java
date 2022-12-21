6fef1c581539dff20bb64f46e105571d5a841b49
MariaSoroks <sormash@gmail.com>
2022-12-21 18:16:19 +0600

    Working with data types and fixing

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -3,6 +3,6 @@ package lang.print.gaps.task4;
 public class FormulaCreator {
     public static void main(String[] args) {
         int a = 1,b=3;
-        float c = (9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2);
+        (9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2);
     }
 }
--- src/main/java/lang/print/gaps/task4/SettingTypes.java
+++ src/main/java/lang/print/gaps/task4/SettingTypes.java
@@ -2,12 +2,12 @@ package lang.print.gaps.task4;
 
 public class SettingTypes {
     public static void main(String[] args) {
-         long first = 10_000_000_000L;
-         char second = 'a';
-         boolean third = 5 > 9;
-         double forth = 12.90434323324342;
-         float fifth = 12.3F;
+         long first;
+         char second;
+         boolean third ;
+         double forth;
+         float fifth;
          byte sixth = 1;
-         int seventh = 129;
+         int seventh;
     }
 }
